7badbd129987b1abe8e3644f615ba41256d43596
Rohit Mahajan <rohitmahajan800737@gmail.com>
2026-03-12 21:37:20 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 import "dotenv/config";
 import app from "./Src/app.js";
 import connectDB from "./Src/config/database.js";
-// import { testAi } from "./Src/services/ai.service.js";
+import { testAi } from "./Src/services/ai.service.js";
 
 
 const PORT = process.env.PORT || 3000;
 
-// testAi();
+testAi();
 connectDB().catch((err) => {
   console.error("MongoDB connection failed:", err);
   process.exit(1);
